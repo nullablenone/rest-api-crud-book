07ed1bf132269a1c14c20c603095194b055f8d89
useryesa9@gmail.com <useryesa9@gmail.com>
2025-03-23 15:12:20 +0700

    first commit

---
+++ config/database.go
@@ -0,0 +1,22 @@
+package config
+
+import (
+	"fmt"
+	"log"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func ConnectDB() *gorm.DB {
+	dsn := "host=localhost user=postgres password=root dbname=db_books port=5432 sslmode=disable"
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatalf("Error connecting to database: %v", err)
+	}
+	fmt.Println("database connected successfully!")
+	DB = db
+	return DB
+}
---
+++ main.go
@@ -0,0 +1,8 @@
+package main
+
+import "rest-api-crud-books/config"
+
+func main(){
+
+	config.ConnectDB()
+}
